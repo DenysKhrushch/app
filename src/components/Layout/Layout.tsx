227f524ca89d84408358c5fa2f5c56f74f8e41a4
DenysKhrushch <denis.hrusch@gmail.com>
2023-04-19 16:57:17 +0200

    Added link to repository

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { AppBar, Paper, Toolbar, Typography } from '@mui/material';
-
+import { AppBar, Link, Paper, Toolbar, Typography } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -12,6 +12,13 @@ export const Layout: React.FC<LayoutProps> = React.memo(({ children }) => (
     <AppBar color="primary" position="static" style={{ height: 64 }}>
       <Toolbar style={{ height: 64 }}>
         <Typography color="inherit">TodoApp by Denys Khrushch</Typography>
+
+        <Link
+          style={{ color: '#fff', marginLeft: 'auto' }}
+          href="https://github.com/DenysKhrushch/app"
+        >
+          <GitHubIcon />
+        </Link>
       </Toolbar>
     </AppBar>
     {children}
